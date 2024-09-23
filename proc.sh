de418dfa7d01b88cde28ddcbe1ed22244ecb500b
ĐӾ3Ø <148585051+Dx3One@users.noreply.github.com>
2024-09-23 09:20:17 +0200

    Update proc.sh

--- proc.sh
+++ proc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DURATION=$((30 * 60)) # 30 Minuten
+read -p "Wie lange möchtest du die Prozesse überwachen? (in Minuten): " DURATION_MINUTES
+DURATION=$((DURATION_MINUTES * 60))
 INTERVAL=10
 HOSTNAME=$(hostname)
 OUTPUT_FILE="pocmon-$HOSTNAME"
 TEMP_FILE=$(mktemp)
-
 monitor_processes() {
     while true; do
         ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 20 >> "$TEMP_FILE"
@@ -12,15 +12,11 @@ monitor_processes() {
         sleep "$INTERVAL"
     done
 }
-
 monitor_processes &
 MONITOR_PID=$!
 sleep "$DURATION"
 kill "$MONITOR_PID"
-
 echo "Prozesse mit der höchsten Last:" >> "$OUTPUT_FILE"
 awk '/%MEM/ {getline} {print $1, $3, $4, $5, $6}' "$TEMP_FILE" | sort -k4 -n -r | uniq -c | sort -n -r | head -n 10 >> "$OUTPUT_FILE"
-
 rm -f "$TEMP_FILE"
-
 echo "Output wurde in der Datei $OUTPUT_FILE gespeichert."
