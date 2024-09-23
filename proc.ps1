871668afc311270f98955cb14b93edc704245671
ĐӾ3Ø <148585051+Dx3One@users.noreply.github.com>
2024-09-23 09:18:55 +0200

    Update proc.ps1

--- proc.ps1
+++ proc.ps1
@@ -1,4 +1,5 @@
-$Duration = 30 * 60 # 30 Minuten
+$DurationInput = Read-Host "Wie lange möchtest du die Prozesse überwachen? (in Minuten)"
+$Duration = [int]$DurationInput * 60
 $Interval = 10
 $Hostname = $env:COMPUTERNAME
 $OutputFile = "pocmon-$Hostname.txt"
@@ -18,12 +19,9 @@ $MonitorJob = Start-Job -ScriptBlock { Monitor-Processes }
 Start-Sleep -Seconds $Duration
 Stop-Job $MonitorJob
 Remove-Job $MonitorJob
-
 Write-Host "Prozesse mit der höchsten Last:" | Out-File -FilePath $OutputFile -Append
 Get-Content $TempFile | Select-String -Pattern "^\d+" | Sort-Object | Group-Object | Sort-Object Count -Descending | Select-Object -First 10 | ForEach-Object {
     $_.Group | Out-File -Append -FilePath $OutputFile
 }
-
 Remove-Item $TempFile
-
 Write-Host "Output wurde in der Datei $OutputFile gespeichert."
